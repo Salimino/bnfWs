ef9c0c07a28198674aca408a2ac42658dcdd70c6
Salimino <salim.chouaf@gmail.com>
2017-10-13 11:34:30 +0200

    Add Maven dependencies

--- bnfWs/src/com/bnf/webServices/lotusWs/bnfLotusWsImpl.java
+++ bnfWs/src/com/bnf/webServices/lotusWs/bnfLotusWsImpl.java
@@ -1,17 +1,39 @@
 package com.bnf.webServices.lotusWs;
 
+import java.io.File;
+
 import javax.jws.WebService;
 
-@WebService (endpointInterface = "com.bnf.webServices.lotusWs.bnfLotusWs")
-public class bnfLotusWsImpl implements bnfLotusWs{
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
 
-	/* (non-Javadoc)
+import com.hraccess.openhr.HRSessionFactory;
+import com.hraccess.openhr.IHRSession;
+import com.hraccess.openhr.exception.SessionBuildException;
+import com.hraccess.openhr.exception.SessionConnectionException;
+
+@WebService(endpointInterface = "com.bnf.webServices.lotusWs.bnfLotusWs")
+public class bnfLotusWsImpl implements bnfLotusWs {
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.bnf.webServices.lotusWs.bnfLotusWs#getLibPoste(java.lang.String)
 	 */
 	@Override
 	public String getLibPoste(String libCode) {
 		// TODO Auto-generated method stub
+		// Creation session OpenHR
+		IHRSession openHrSession;
+		File openHrConfFile = new File("/Users/salimchouaf/git/bnfWs/bnfWs/conf/openhr.properties");
+		try {
+			Configuration openHrSessionConf = new PropertiesConfiguration(openHrConfFile);
+			openHrSession = HRSessionFactory.getFactory().createSession(openHrSessionConf);
+		} catch (ConfigurationException | SessionBuildException | SessionConnectionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return "Libelle TOTO";
 	}
-
 }
--- bnfWs/src/com/bnf/webServices/lotusWs/pblisher/bnfLotusWsPublisher.java
+++ bnfWs/src/com/bnf/webServices/lotusWs/pblisher/bnfLotusWsPublisher.java
@@ -6,6 +6,6 @@ import com.bnf.webServices.lotusWs.bnfLotusWsImpl;
 public class bnfLotusWsPublisher {
 
 	public static void main(String[] args) {
-		Endpoint.publish("http://localhost:4547/services/lotusWs", new bnfLotusWsImpl());
+		Endpoint.publish("http://localhost:4549/services/lotusWs", new bnfLotusWsImpl());
 	}
 }
